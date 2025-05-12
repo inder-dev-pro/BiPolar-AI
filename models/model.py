fb9f1baa5e665f550002d97fec2dc53ea82086a2
inder-dev-pro <inder.31ssb@gmail.com>
2025-05-12 19:03:52 +0530

    Models update!

--- models/model.py
+++ models/model.py
@@ -8,4 +8,4 @@ def get_model(num_classes):
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     # Replace the pre-trained head with a new one (for Pascal VOC)
     model.roi_heads.box_predictor = torchvision.models.detection.faster_rcnn.FastRCNNPredictor(in_features, num_classes)
-    return model 
+    return model
